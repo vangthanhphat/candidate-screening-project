759b94aa2e0f698fd3aeaf9dd5703f6604e45635
unknown <orca413>
2026-04-14 10:56:36 +0700

    firt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Typography, Paper } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm" style={{ marginTop: '50px' }}>
+      <Paper elevation={3} style={{ padding: '30px', borderRadius: '10px' }}>
+        <Typography variant="h4" align="center" color="primary" fontWeight="bold" gutterBottom>
+          My Shopping List
+        </Typography>
+        <Typography variant="body1" align="center" color="textSecondary">
+          Dự án đầu tay của mình!
+        </Typography>
+      </Paper>
+    </Container>
   );
 }
 
